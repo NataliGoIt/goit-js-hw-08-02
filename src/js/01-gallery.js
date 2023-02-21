215010f029228ce9d966225f5348c232eb3a0f00
NataliGoIt <nataliakal4enko@gmail.com>
2023-02-21 13:40:34 +0200

    image-simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from "./gallery-items";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+// console.log(gallery);
+function galleryMarkUp(items) {
+  const markup = items
+    .map(
+      ({
+        preview,
+        original,
+        description,
+      } = items) => `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`
+    )
+    .join("");
+  gallery.innerHTML = markup;
+}
+
+galleryMarkUp(galleryItems);
+
+new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "botton",
+});
